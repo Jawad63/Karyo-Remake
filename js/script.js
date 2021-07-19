97d5a7b4c8fa1e489c2dd75103928334506ef611
Jawad63 <jawadqureshi111@gmail.com>
2021-07-19 22:26:43 +0200

    structure and positioning for filter gallery completed

--- js/script.js
+++ js/script.js
@@ -14,7 +14,33 @@ $(document).ready(function(){
 /* "rgba(0,0,0,.5)"  replace this with "white in order to get that dark transparent color and remove the else
 comment." */
 
-
+$('.filter-button').on('click', (e) => {
+    const filter = $(e.target).attr('data-filter');
+    console.log(filter);
+       const items = $('.item-content').parent();
+    for (item of items) {
+      if (filter == '') {
+        
+          $(item).addClass('animated zoomIn faster');
+        $(item).parent().addClass('animated zoomIn faster');
+        $(item).removeClass('d-none');
+        $(item).parent().removeClass('d-none');
+        
+       
+        console.log('x');
+      }else if ($(item).attr('data-category') == filter) {
+        $(item).addClass('animated zoomIn faster');
+        $(item).parent().addClass('animated zoomIn faster');
+        $(item).removeClass('d-none');
+        $(item).parent().removeClass('d-none');
+      } else {
+        $(item).addClass('d-md-none  d-lg-block');
+        $(item).parent().addClass('d-none');
+        $(item).removeClass('animated zoomIn faster');
+        $(item).parent().removeClass('animated zoomIn faster');
+      }
+    }
+  });
 
 
 
