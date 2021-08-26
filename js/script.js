55ecbd9322b81dfa3328b2b0f64cd54d5f1c5404
Jawad63 <jawadqureshi111@gmail.com>
2021-08-27 01:04:07 +0200

    fixed the parallex effect for local host

--- js/script.js
+++ js/script.js
@@ -8,9 +8,7 @@ $(document).ready(function () {
     else {
         $(".navbar").css({ "background-color": "rgba(255, 255, 255, 0.5)" });
     }
-    
     });
-
 });
 
 /* "rgba(0,0,0,.5)"  replace this with "white in order to get that dark transparent color and remove the else
