aa536ef0ab5c7d367e232c76db49ebb47ce3ebea
Jawad63 <jawadqureshi111@gmail.com>
2021-08-28 19:28:48 +0200

    karyo remake 1.0

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
     $(window).scroll(function(){
     if($(window).scrollTop()>100)
-    $(".navbar").css({"background-color" : "rgba(0,0,0,.5)"});
+    $(".navbar").css({"background-color" : "rgba(255, 255, 255, 0.5)"});
     else {
         $(".navbar").css({ "background-color": "rgba(255, 255, 255, 0.5)" });
     }
