c9297d45cfe5074ca978b8c0217358e3fabedac8
Jawad63 <jawadqureshi111@gmail.com>
2021-09-08 08:17:30 +0200

    changes made for diensten section. :incomplete

--- js/script.js
+++ js/script.js
@@ -150,6 +150,91 @@ $(function () {
 });
 
 
+// Team section:
+// local reviews data
+const reviews = [
+    {
+      id: 1,
+      name: "Yusuf Yorulmaz",
+      job: "Zaakvoerder",
+      img:
+        "Images/Team/Yusuf-team.jpeg",
+      text:
+        "Sinds 1992 staat Yusuf zelf op de werkvloer, gestart bij een baas en sinds 2003 staat hij zelf aan het roer.",
+    },
+    {
+      id: 2,
+      name: "Verkoopsdienst",
+      job: "Gratis offerte",
+      img:
+        "images/logo's/Karyo_Small_Blue-removebg-preview.png",
+      text:
+        "Voor een offerte op maat, graag het online formulier invullen zodat ons team u kan contacteren.",
+    },
+    {
+      id: 3,
+      name: "Na Service",
+      job: "Gratis offerte",
+      img:
+        "images/logo's/Karyo_Small_Blue-removebg-preview.png",
+      text:
+            "Voor een offerte op maat, graag het online formulier invullen zodat ons team u kan contacteren.",
+    },
+  ];
+  // select items
+  const img = document.getElementById("person-img");
+  const author = document.getElementById("author");
+  const job = document.getElementById("job");
+  const info = document.getElementById("info");
+  
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+  const randomBtn = document.querySelector(".random-btn");
+  
+  // set starting item
+  let currentItem = 0;
+  
+  // load initial item
+  window.addEventListener("DOMContentLoaded", function () {
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+  });
+  
+  // show person based on item
+  function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+  }
+  // show next person
+  nextBtn.addEventListener("click", function () {
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+      currentItem = 0;
+    }
+    showPerson(currentItem);
+  });
+  // show prev person
+  prevBtn.addEventListener("click", function () {
+    currentItem--;
+    if (currentItem < 0) {
+      currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+  });
+  // show random person
+  randomBtn.addEventListener("click", function () {
+    console.log("hello");
+  
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson(currentItem);
+  });
+
 
 
 
