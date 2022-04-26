5d71587444d5f3214b40a06fefef6290a6456d31
Jawad63 <jawadqureshi111@gmail.com>
2022-04-26 13:51:07 +0200

    making filter text gallery part 1

--- js/script.js
+++ js/script.js
@@ -24,19 +24,26 @@ $('.filter-button').on('click', (e) => {
     for (item of items) {
       if (filter == '') {
         
-          $(item).addClass('animated zoomIn faster');
-        $(item).parent().addClass('animated zoomIn faster');
-        $(item).removeClass('d-none');
-        $(item).parent().removeClass('d-none');
-        
-       
-        console.log('x');
-      }else if ($(item).attr('data-category') == filter) {
         $(item).addClass('animated zoomIn faster');
         $(item).parent().addClass('animated zoomIn faster');
         $(item).removeClass('d-none');
         $(item).parent().removeClass('d-none');
-      } else {
+        console.log('x');
+      }
+      
+      else if ($(item).attr('data-category') == filter) {
+
+        $('#' + filter.replaceAll(" ", "-")).attr('style', 'display:block');
+         // console.log('#' + filter.replaceAll(" ", "-"));
+
+          
+        $(item).addClass('animated zoomIn faster');
+        $(item).parent().addClass('animated zoomIn faster');
+        $(item).removeClass('d-none');
+        $(item).parent().removeClass('d-none');
+      }
+      
+      else {
         $(item).addClass('d-md-none  d-lg-block');
         $(item).parent().addClass('d-none');
         $(item).removeClass('animated zoomIn faster');
