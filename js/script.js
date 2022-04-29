a92de53b63dc36000617d3b60981829d5cf63d8a
Jawad63 <jawadqureshi111@gmail.com>
2022-04-29 14:28:13 +0200

    /././.

--- js/script.js
+++ js/script.js
@@ -3,10 +3,13 @@
 $(document).ready(function () {
 
     $(window).scroll(function(){
-    if($(window).scrollTop()>100)
-    $(".navbar").css({"background-color" : "rgba(255, 255, 255, 0.5)"});
+        if ($(window).scrollTop() > 100)
+            $(".navbar").css({ "background-color": "rgba(255, 255, 255, 0.7)" })
+            
     else {
-        $(".navbar").css({ "background-color": "rgba(255, 255, 255, 0.5)" });
+        $(".navbar").css({ "transition": "2s ease"});
+            $(".navbar").css({ "background-color": "rgba(255, 255, 255, 0.7)" });
+
     }
     });
 });
